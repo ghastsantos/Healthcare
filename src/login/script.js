25ef0af930c634c6740fc539b7fed988dacf8645
Gastao Eduardo Santos Borges <gastao.borges@pucpr.edu.br>
2024-09-02 16:41:43 -0300

    first commit

---
+++ src/login/script.js
@@ -0,0 +1,19 @@
+let form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let dados = document.querySelectorAll('#dado');
+    let validacao = document.querySelectorAll('#campoVazio')
+    for (let i = 0; i < dados.length; i++){
+        if (dados[i].value === ""){
+            console.log('Valor inválido no campo:', dados[i].placeholder);
+            validacao[i].innerHTML = 'Login ou senha inválidos'
+            dados[i].style.borderColor = 'red';
+            break
+        } else {
+            console.log('O campo foi preenchido:', dados[i].placeholder);
+            validacao[i].innerHTML = ''
+            dados[i].style.borderColor = '';
+        }
+    }
+});
